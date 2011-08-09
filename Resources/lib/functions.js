98ced2cca083a3b4be2f9e8e9edca65a5f1c2c12
samuele coppede <boardmain@gmail.com>
2011-08-09 12:33:23 +0200

    prove

--- Resources/lib/functions.js
+++ Resources/lib/functions.js
@@ -18,7 +18,7 @@
 				error:function(error)
 				{
 					// create alert
-				/*	var a = Titanium.UI.createAlertDialog({title:'Video'});
+					var a = Titanium.UI.createAlertDialog({title:'Video'});
 	
 					// set message
 					if (error.code == Titanium.Media.NO_VIDEO)
@@ -31,8 +31,7 @@
 					}
 	
 					// show alert
-					a.show();*/
-					myApp.uploadDialog('mymovie.mov');
+					a.show();
 				},
 				mediaTypes: Titanium.Media.MEDIA_TYPE_VIDEO,
 				videoMaximumDuration:600000,
@@ -44,18 +43,90 @@
 			var style = Ti.UI.iPhone.MODAL_TRANSITION_STYLE_FLIP_HORIZONTAL;
 			var presentation = Ti.UI.iPhone.MODAL_PRESENTATION_FORMSHEET;
 			var w = Ti.UI.createWindow({
-				backgroundColor:'purple'
+				backgroundColor:'white'
 			});
+			
+			var l_Video = Titanium.UI.createLabel({
+				top:10,
+				left:10,
+				width:50,
+				height:35,
+				font:{fontSize:18, fontWeight:'bold'},
+				text:'Video'
+			});
+			var Video = Titanium.UI.createTextField({
+				height:35,
+				top:10,
+				left:60,
+				width:200,
+				borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+				value:nomevideo,
+				font:{fontSize:20, fontWeight:'bold'},
+				color:'#888',
+				editable:false
+			});
+	
+			w.add(l_Video);
+			w.add(Video);
+			
+			
+			var l_Params = Titanium.UI.createLabel({
+				text:'Parametri',
+				font:{fontSize:18, fontWeight:'bold'},
+				left:10,
+				top:50,
+				width:300,
+				height:'auto'
+			});
+			w.add(l_Params);
+			
+			
+			var ta1 = Titanium.UI.createTextArea({
+				editable: true,
+				value:'nomeparam1=valore1;nomeparam2=valore2',
+				height:200,
+				width:300,
+				top:80,
+				font:{fontSize:20, fontWeight:'bold'},
+				color:'#888',
+				textAlign:'left',
+				appearance:Titanium.UI.KEYBOARD_APPEARANCE_ALERT,	
+				keyboardType:Titanium.UI.KEYBOARD_NUMBERS_PUNCTUATION,
+				returnKeyType:Titanium.UI.RETURNKEY_EMERGENCY_CALL,
+				borderWidth:2,
+				borderColor:'#bbb',
+				borderRadius:5,
+				suppressReturn:false
+				
+			});
+			w.add(ta1);
+
+
 			var b = Ti.UI.createButton({
-				title:'Close '+nomevideo,
+				title:'Chiudi',
 				width:100,
-				height:30
+				height:30,
+				top:400,
+				left:50
 			});
 			b.addEventListener('click',function()
 			{
 				w.close();
 			});
+			
+			var u = Ti.UI.createButton({
+				title:'Upload',
+				width:100,
+				height:30,
+				top:400,
+				left:200,
+			});
+			u.addEventListener('click',function()
+			{
+				alert('upload');
+			});
 			w.add(b);
+			w.add(u);
 			w.open({modal:true,modalTransitionStyle:style,modalStyle:presentation,navBarHidden:true});
 	};
 	
